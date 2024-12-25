57c8397fa486c3b09c7976bc905069c3953695b6
Wooil <6431312+wooilshin@users.noreply.github.com>
2024-12-26 05:06:27 +0900

    Add files via upload

--- train.py
+++ train.py
@@ -4,21 +4,44 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model import ImageClassifier
-from trainer import Trainer
-from data_loader import get_loaders
+from simple_ntc.trainer import Trainer
+from simple_ntc.data_loader import DataLoader
+
+from simple_ntc.models.rnn import RNNClassifier
+from simple_ntc.models.cnn import CNNClassifier
+
 
 def define_argparser():
+    '''
+    Define argument parser to set hyper-parameters.
+    '''
     p = argparse.ArgumentParser()
 
     p.add_argument('--model_fn', required=True)
-    p.add_argument('--gpu_id', type=int, default=0 if torch.cuda.is_available() else -1)
+    p.add_argument('--train_fn', required=True)
+    
+    p.add_argument('--gpu_id', type=int, default=-1)
+    p.add_argument('--verbose', type=int, default=2)
 
-    p.add_argument('--train_ratio', type=float, default=.8)
+    p.add_argument('--min_vocab_freq', type=int, default=5)
+    p.add_argument('--max_vocab_size', type=int, default=999999)
 
     p.add_argument('--batch_size', type=int, default=256)
-    p.add_argument('--n_epochs', type=int, default=20)
-    p.add_argument('--verbose', type=int, default=2)
+    p.add_argument('--n_epochs', type=int, default=10)
+
+    p.add_argument('--word_vec_size', type=int, default= 256)
+    p.add_argument('--dropout', type=float, default=.3)
+
+    p.add_argument('--max_length', type=int, default=256)
+    
+    p.add_argument('--rnn', action='store_true')
+    p.add_argument('--hidden_size', type=int, default=512)
+    p.add_argument('--n_layers', type=int, default=4)
+
+    p.add_argument('--cnn', action='store_true')
+    p.add_argument('--use_batch_norm', action='store_true')
+    p.add_argument('--window_sizes', type=int, nargs='*', default=[3, 4, 5])
+    p.add_argument('--n_filters', type=int, nargs='*', default=[100, 100, 100])
 
     config = p.parse_args()
 
@@ -26,21 +49,88 @@ def define_argparser():
 
 
 def main(config):
-    # Set device based on user defined configuration.
-    device = torch.device('cpu') if config.gpu_id < 0 else torch.device('cuda:%d' % config.gpu_id)
+    loaders = DataLoader(
+        train_fn=config.train_fn,
+        batch_size=config.batch_size,
+        min_freq=config.min_vocab_freq,
+        max_vocab=config.max_vocab_size,
+        device=config.gpu_id
+    )
 
-    train_loader, valid_loader, test_loader = get_loaders(config)
+    print(
+        '|train| =', len(loaders.train_loader.dataset),
+        '|valid| =', len(loaders.valid_loader.dataset),
+    )
+    
+    vocab_size = len(loaders.text.vocab)
+    n_classes = len(loaders.label.vocab)
+    print('|vocab| =', vocab_size, '|classes| =', n_classes)
 
-    print("Train:", len(train_loader.dataset))
-    print("Valid:", len(valid_loader.dataset))
-    print("Test:", len(test_loader.dataset))
+    if config.rnn is False and config.cnn is False:
+        raise Exception('You need to specify an architecture to train. (--rnn or --cnn)')
 
-    model = ImageClassifier(28**2, 10).to(device)
-    optimizer = optim.Adam(model.parameters())
-    crit = nn.NLLLoss()
+    if config.rnn:
+        # Declare model and loss.
+        model = RNNClassifier(
+            input_size=vocab_size,
+            word_vec_size=config.word_vec_size,
+            hidden_size=config.hidden_size,
+            n_classes=n_classes,
+            n_layers=config.n_layers,
+            dropout_p=config.dropout,
+        )
+        optimizer = optim.Adam(model.parameters())
+        crit = nn.NLLLoss()
+        print(model)
 
-    trainer = Trainer(config)
-    trainer.train(model, crit, optimizer, train_loader, valid_loader)
+        if config.gpu_id >= 0:
+            model.cuda(config.gpu_id)
+            crit.cuda(config.gpu_id)
+
+        rnn_trainer = Trainer(config)
+        rnn_model = rnn_trainer.train(
+            model,
+            crit,
+            optimizer,
+            loaders.train_loader,
+            loaders.valid_loader
+        )
+    if config.cnn:
+        # Declare model and loss.
+        model = CNNClassifier(
+            input_size=vocab_size,
+            word_vec_size=config.word_vec_size,
+            n_classes=n_classes,
+            use_batch_norm=config.use_batch_norm,
+            dropout_p=config.dropout,
+            window_sizes=config.window_sizes,
+            n_filters=config.n_filters,
+        )
+        optimizer = optim.Adam(model.parameters())
+        crit = nn.NLLLoss()
+        print(model)
+
+        if config.gpu_id >= 0:
+            model.cuda(config.gpu_id)
+            crit.cuda(config.gpu_id)
+
+        cnn_trainer = Trainer(config)
+        cnn_model = cnn_trainer.train(
+            model,
+            crit,
+            optimizer,
+            loaders.train_loader,
+            loaders.valid_loader
+        )
+
+    torch.save({
+        'rnn': rnn_model.state_dict() if config.rnn else None,
+        'cnn': cnn_model.state_dict() if config.cnn else None,
+        'config': config,
+        'vocab': loaders.text.vocab,
+        'classes': loaders.label.vocab,
+    }, config.model_fn)
+
 
 if __name__ == '__main__':
     config = define_argparser()
